bd81abc9570fb89218033d08e4ccd53ddb92e5ee
Abhimanyu Pathania <pathania.abhimanyu@gmail.com>
2021-09-24 00:26:15 +0530

    feat: add marketing module

---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,20 @@
+module.exports = {
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
---
+++ packages/marketing/src/bootstrap.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const mount = (el) => {
+  ReactDOM.render(
+    <h1>Marketing App</h1>, el
+  )
+};
