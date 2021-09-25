b6a1b8fda0bae951da1004588d5a9c330b6f237e
Abhimanyu Pathania <pathania.abhimanyu@gmail.com>
2021-09-26 01:31:12 +0530

    fix: add prefix to prod css classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import MarketingWrapper from "./components/MarketingWrapper";
 import Header from "./components/Header";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "marketing",
+});
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingWrapper />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingWrapper />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 }
