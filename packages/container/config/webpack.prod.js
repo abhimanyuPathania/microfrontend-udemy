2ec15821d4c933358e40c8ba3f7c9c3e34a9c2ab
Abhimanyu Pathania <pathania.abhimanyu@gmail.com>
2021-09-25 17:20:47 +0530

    feat: add container prod config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,2 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
+const PRODUCTION_DOMAIN = process.env.PRODUCTION_DOMAIN;
+const devConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${PRODUCTION_DOMAIN}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
+module.exports = merge(commonConfig, devConfig);
