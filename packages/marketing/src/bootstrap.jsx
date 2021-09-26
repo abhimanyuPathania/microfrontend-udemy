1f47f62ec332b44c9c374dc76fd9aab16faa9007
Abhimanyu Pathania <pathania.abhimanyu@gmail.com>
2021-09-26 19:28:49 +0530

    feat(marketing): use browser history when running solo

--- packages/marketing/src/bootstrap.jsx
+++ packages/marketing/src/bootstrap.jsx
@@ -5,25 +5,25 @@ import { createMemoryHistory } from "history";
 import { syncNavigation, shouldSyncNavigation } from "../../common/utilties";
 import App from "./App";
 
-export const mount = (el, { onNavigation }) => {
+export const mount = (el, { onNavigation, defaultHistory }) => {
   const memoryHistory = createMemoryHistory({
     initialEntries: [window.location.pathname],
   });
+  const history = defaultHistory || memoryHistory;
 
-  if (onNavigation) memoryHistory.listen(onNavigation);
+  if (onNavigation) history.listen(onNavigation);
 
-  ReactDOM.render(<App history={memoryHistory} />, el);
+  // defaultHistory is the BrowserHitsory injected when running in isolation
+  ReactDOM.render(<App history={history} />, el);
   // Object exposed to container
   return {
     onParentNavigation: (location, method) => {
-      if (
-        shouldSyncNavigation({ nextLocation: location, history: memoryHistory })
-      ) {
+      if (shouldSyncNavigation({ nextLocation: location, history })) {
         console.log("marketing >> onParentNavigation:", method, location);
         syncNavigation({
           nextLocation: location,
           nextMethod: method,
-          history: memoryHistory,
+          history,
         });
       }
     },
--- packages/marketing/src/bootstrap.mock.js
+++ packages/marketing/src/bootstrap.mock.js
@@ -1,3 +1,8 @@
+import { createBrowserHistory } from "history";
+
 import { mount } from "./bootstrap";
 
-mount(document.querySelector("#mock-marketing"), {});
+mount(document.querySelector("#mock-marketing"), {
+  // Use browser history when running app in isolation
+  defaultHistory: createBrowserHistory(),
+});
